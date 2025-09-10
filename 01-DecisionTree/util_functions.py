2e9af5839ffd0b283c98b2a9a451fb315d171a46
Roberto Doriguzzi Corin <rdoriguzzi@fbk.eu>
2025-09-10 11:53:01 +0200

    updated DT demo

--- 01-DecisionTree/util_functions.py
+++ 01-DecisionTree/util_functions.py
@@ -28,13 +28,11 @@ TRAIN_SIZE = 0.90 # size of the training set wrt the total number of samples
 protocols = ['arp','data','dns','ftp','http','icmp','ip','ssdp','ssl','telnet','tcp','udp']
 powers_of_two = np.array([2**i for i in range(len(protocols))])
 
-
-def get_feature_list(time_window=10,max_flow_len=10):
+def get_feature_list_flatten(time_window=10,max_flow_len=10):
     # feature list with min and max values
     feature_list = OrderedDict([
         ('timestamp', [0,time_window]),
         ('packet_length',[0,1<<16]),
-        #('highest_layer',[0,1<<32]),
         ('IP_flags_df',[0,max_flow_len]),
         ('IP_flags_mf',[0,max_flow_len]),
         ('IP_flags_rb',[0,max_flow_len]),
@@ -53,21 +51,53 @@ def get_feature_list(time_window=10,max_flow_len=10):
         ('TCP_window_size',[0,1<<16]),
         ('UDP_length',[0,1<<16]),
         ('ICMP_type',[0,1<<8]),
-        ('Packets',[0,10])]
+        ('Packets',[0,max_flow_len])]
     )
     return feature_list
 
-def get_feature_names():
-    features_names_list = get_feature_list().keys()
+def get_feature_list_array(time_window=10,max_flow_len=10):
+    # feature list with min and max values
+    feature_list = OrderedDict([
+        ('timestamp', [0,time_window]),
+        ('packet_length',[0,1<<16]),
+        ('IP_flags_df',[0,1]),
+        ('IP_flags_mf',[0,1]),
+        ('IP_flags_rb',[0,1]),
+        ('IP_frag_off',[0,1<<13]),
+        ('protocols',[0,1<<len(protocols)]),
+        ('TCP_length',[0,1<<16]),
+        ('TCP_flags_ack',[0,1]),
+        ('TCP_flags_cwr',[0,1]),
+        ('TCP_flags_ece',[0,1]),
+        ('TCP_flags_fin',[0,1]),
+        ('TCP_flags_push',[0,1]),
+        ('TCP_flags_res',[0,1]),
+        ('TCP_flags_reset',[0,1]),
+        ('TCP_flags_syn',[0,1]),
+        ('TCP_flags_urg',[0,1]),
+        ('TCP_window_size',[0,1<<16]),
+        ('UDP_length',[0,1<<16]),
+        ('ICMP_type',[0,1<<8])]
+    )
+    return feature_list
+
+def get_feature_names(flatten):
+    if flatten == True:
+        features_names_list = get_feature_list_flatten().keys()
+    else:
+        features_names_list = get_feature_list_array().keys()
     return list(features_names_list)
 
-def load_dataset(path):
+def load_dataset(path,channels=False):
     filename = glob.glob(path)[0]
     dataset = h5py.File(filename, "r")
     set_x_orig = np.array(dataset["set_x"][:])  # features
     set_y_orig = np.array(dataset["set_y"][:])  # labels
 
-    X_train = set_x_orig
+    if channels == True:
+        X_train = np.reshape(set_x_orig, (set_x_orig.shape[0], set_x_orig.shape[1], set_x_orig.shape[2], 1))
+    else:
+        X_train = set_x_orig
     Y_train = set_y_orig#.reshape((1, set_y_orig.shape[0]))
 
     return X_train, Y_train
@@ -76,10 +106,14 @@ def scale_linear_bycolumn(rawpoints, mins,maxs,high=1.0, low=0.0):
     rng = maxs - mins
     return high - (((high - low) * (maxs - rawpoints)) / rng)
 
-def count_packets_in_dataset(X_list):
+def count_packets_in_dataset(X_list, flatten=True):
     packet_counters = []
     for X in X_list:
-        packet_counters.append(int(np.sum(X[:,20])))
+        if (flatten == False):
+            TOT = X.sum(axis=2)
+            packet_counters.append(np.count_nonzero(TOT))
+        else:
+            packet_counters.append(int(np.sum(X[:,20])))
 
     return packet_counters
 
@@ -87,8 +121,11 @@ def all_same(items):
     return all(x == items[0] for x in items)
 
 # min/max values of features based on the nominal min/max values of the single features (as defined in the feature_list dict)
-def static_min_max(time_window=10,max_flow_len=10):
-    feature_list = get_feature_list(time_window,max_flow_len)
+def static_min_max(flatten, time_window=10,max_flow_len=10):
+    if flatten == True:
+        feature_list = get_feature_list_flatten(time_window,max_flow_len)
+    else:
+        feature_list = get_feature_list_array(time_window,max_flow_len)
 
     min_array = np.zeros(len(feature_list))
     max_array = np.zeros(len(feature_list))
@@ -178,3 +215,36 @@ def flatten_samples(X):
         new_sample = np.array(new_sample)
         X_new.append(new_sample)
     return X_new
+
+# def flatten_samples(X):
+#     X_new = []
+#     for sample in X:
+#         new_sample = []
+#         time_feature = np.mean(np.ediff1d(sample[:,0],to_begin=0)) #mean of the differences between consecutive elements of an array.
+#         new_sample.append(time_feature)
+#         packet_len = np.mean(sample[:,1])
+#         new_sample.append(packet_len)
+#         highest_layer = np.mean(sample[:,2])
+#         new_sample.append(highest_layer)
+#         ip_flags = list(np.sum(sample[:,3:6],axis=0)) # we take the sum of each flag
+#         new_sample = new_sample + ip_flags
+#         frag_off = np.mean(sample[:,6])
+#         new_sample.append(frag_off)
+#         protocols = np.mean(sample[:,7])
+#         new_sample.append(protocols)
+#         tcp_len = np.mean(sample[:,8])
+#         new_sample.append(tcp_len)
+#         tcp_flags = list(np.sum(sample[:,9:18],axis=0)) # we take the sum of each flag
+#         new_sample = new_sample + tcp_flags
+#         tcp_win_size = np.mean(sample[:,18])
+#         new_sample.append(tcp_win_size)
+#         udp_len = np.mean(sample[:,19])
+#         new_sample.append(udp_len)
+#         icmp_type = np.mean(sample[:,20])
+#         new_sample.append(icmp_type)
+#         packets_nr = sample.shape[0] #number of packets in the sample
+#         new_sample.append(packets_nr)
+
+#         new_sample = np.array(new_sample)
+#         X_new.append(new_sample)
+#     return X_new
